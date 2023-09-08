31ad2d91ebeee22b7cc4e57cf1d382bd65f8c304
vishal0036 <vishal@fanaticcoders.com>
2023-09-08 16:22:08 +0530

    oopss file uploaded

---
+++ opps.php
@@ -0,0 +1,84 @@
+<?php 
+
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit();
+$apple->set_name('Apple');
+$apple->set_color('Red');
+echo "Name: " . $apple->get_name();
+echo "<br>";
+echo "Color: " . $apple->get_color()."<br>";
+
+
+
+
+
+
+for($i=0;$i<=5;$i++){  
+  for($j=5-$i;$j>=1;$j--){  
+  echo "* ";  
+  }  
+  echo "<br>";  
+  }  
+
+
+
+
+
+
+
+  echo readfile('phpmain.php');
+
+
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+
+  throw new Exception("This is an example exception.");
+
+
+
+
+
+  class transection{
+    private float $amount;
+   public function _conta
+
+
+
+
+  }
+
+
+
+  interface animal{
+    public function makesound();
+   
+  }
+  class Cat impliments animal{
+    public function makesound(){
+      echo "meao";
+    }
+  }
+ 
+
+
+
